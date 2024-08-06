e245731359f89d406c72357edadffcab1124be6a
Tsvi Mostovicz <ttmost@gmail.com>
2024-08-06 16:49:45 +0300

    Accept args

--- demo-app/talk.py
+++ demo-app/talk.py
@@ -1,8 +1,11 @@
+import argparse
 import json
 from pathlib import Path
 
 def get_data(filename: Path):
-    return json.loads(open(filename).read())
+    if filename.suffix == ".json":
+        return json.loads(open(filename).read())
+    raise ValueError(f"Unsupported file format: {filename.suffix}")
 
 def generate_report(data: list[dict]):
     key_sizes = {key: max(len(str(entry[key])) for entry in data) for key in data[0].keys()}
@@ -16,7 +19,10 @@ def generate_report(data: list[dict]):
         print("")
 
 def main():
-    data = get_data(filename=Path(__file__).parent / "data.json")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=Path, required=True)
+    args = parser.parse_args()
+    data = get_data(args.filename)
     generate_report(data)
 
 if __name__ == "__main__":
